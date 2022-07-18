45640266c41a9d757aaa179e9d665b60e07fe785
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-07-18 13:12:40 -0700

    change the file structures

--- javascripts/above-body-nav-title/connectors/discovery-list-controls-above/above-body-nav-title.js
+++ javascripts/above-body-nav-title/connectors/discovery-list-controls-above/above-body-nav-title.js
@@ -5,9 +5,9 @@ export default {
   setupComponent(attrs, component) {
       withPluginApi("0.11", (api) => {
         api.onPageChange(() => {
-          getOwner(this).lookup("router:main")
-          .then(res => console.log(res));
-          this.set("currentRouteName", router.currentRouteName)
+          const router = getOwner(this).lookup("router:main");
+          console.log(router);
+          this.set("currentRouteName", router.currentRouteName);
         });
       });
   },
