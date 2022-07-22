ab24a160386ebf608d3204d9b5cc99f0155a718a
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-07-22 11:57:37 -0700

    add topic button

--- javascripts/above-body-nav-title/connectors/discovery-list-controls-above/above-body-nav-title.js
+++ javascripts/above-body-nav-title/connectors/discovery-list-controls-above/above-body-nav-title.js
@@ -24,10 +24,17 @@ export default {
           } 
 
           const realQuestionBtn = document.querySelector('#create-topic');
-          const visibleQUestionBtn = document.querySelector('#create-topic');
+          const visibleQuestionBtn = document.querySelector('#fake-create-topic');
 
-          if (realQuestionBtn) {
-
+          if (realQuestionBtn && visibleQuestionBtn) {
+            const btnTextVisible = visibleQuestionBtn.querySelector(".btn-text");
+            const btnTextReal = realQuestionBtn.querySelector(".d-button-label");
+            visibleQuestionBtn.addEventListener('click', e => {
+              realQuestionBtn.click();
+              if (btnTextReal.innerHTML !== btnTextVisible.innerHTML) {
+                btnTextVisible.innerHTML = btnTextReal.innerHTML;
+              }
+            })
           }
         });
       });
