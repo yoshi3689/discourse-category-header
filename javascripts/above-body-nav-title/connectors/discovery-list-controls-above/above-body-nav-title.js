2fc0165d6bf0d35649f3fb764177ddb7a8591cfe
KeYang <kyang72@my.bcit.ca>
2022-07-22 17:18:06 -0700

    changing sorting button layout

--- javascripts/above-body-nav-title/connectors/discovery-list-controls-above/above-body-nav-title.js
+++ javascripts/above-body-nav-title/connectors/discovery-list-controls-above/above-body-nav-title.js
@@ -2,7 +2,31 @@ import { withPluginApi } from "discourse/lib/plugin-api";
 import { getOwner } from "discourse-common/lib/get-owner";
 
 export default {
+  shouldRender(args, component) {
+    const router = getOwner(this).lookup("router:main");
+
+    if (
+      !component.siteSettings.show_filter_by_solved_status ||
+      router.currentPath === "discovery.categories"
+    ) {
+      return false;
+    } else if (component.siteSettings.allow_solved_on_all_topics) {
+      return true;
+    } else {
+      const controller = getOwner(this).lookup(
+        "controller:navigation/category"
+      );
+      return controller && controller.get("category.enable_accepted_answers");
+    }
+  },
   setupComponent(attrs, component) {
+    const statuses = ["likes", "views", "activities"].map((status) => {
+      return {
+        name: status,
+        value: status,
+      };
+    });
+    component.set("statuses", statuses);
       withPluginApi("0.11", (api) => {
         api.onPageChange(() => {
           const router = getOwner(this).lookup("router:main");
@@ -35,5 +59,16 @@ export default {
           }
         });
       });
+  },
+
+  actions: {
+  
+    changeStatus(newStatus) {
+      
+        const router = getOwner(this).lookup("router:main");
+        router.transitionTo({ queryParams: { order: newStatus } });
+      
+      
+    },
   },
 };
